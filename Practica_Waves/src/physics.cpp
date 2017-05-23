a48391c933cf68a326dd4f6246ad1874e9dd37e6
Unknown <yoshimcreary@hotmail.com>
2017-05-23 11:29:15 +0200

    XD

--- Practica_Waves/src/physics.cpp
+++ Practica_Waves/src/physics.cpp
@@ -11,7 +11,7 @@
 #include <vector>
 #include <forward_list>
 #include <map>
-
+#include <math.h>
 
 bool show_test_window = false;
 int updateRange = 20;
@@ -41,7 +41,7 @@ float cont = 0;
 #define TEST 10.f;
 
 glm::vec3 normal;
-glm::vec3 gravity = glm::vec3(0, -9.8, 0);
+float gravity = -9.81f;
 
 struct Particle {
 	glm::vec3 pos;
@@ -51,6 +51,18 @@ struct Particle {
 	glm::vec3 lastVel;
 	glm::vec3 force;
 };
+
+struct OurShpere {
+	glm::vec3 pos;
+	glm::vec3 vel;
+	float rad;
+	float mass = 20;
+	glm::vec3 linealMomentum;
+	glm::vec3 force;
+
+};
+
+OurShpere *sphere = new OurShpere();
 
 struct Waves {
 	float amplitude;
@@ -70,7 +82,7 @@ void GUI() {
 		//GUI Waterfall
 	
 		ImGui::SliderFloat("Time", &cont, 0, 20);
-		
+		ImGui::SliderFloat("Mass", &sphere->mass, 1, 300);
 	}
 
 	// ImGui test window. Most of the sample code is in ImGui::ShowTestWindow()
@@ -103,23 +115,9 @@ namespace Sphere {
 
 }
 
-
-struct OurShpere {
-	glm::vec3 pos;
-	glm::vec3 vel;
-	float rad;
-	float mass = 3;
-	glm::vec3 linealMomentum;
-	glm::vec3 force;
-
-};
-
-
-
 float *mesh = new float[3 * maxMesh];
 Particle *pC = new Particle[LilSpheres::maxParticles];
-OurShpere *sphere = new OurShpere();
-
+float cD = 1.0;
 
 namespace GerstnerWaves {
 
@@ -144,8 +142,9 @@ namespace GerstnerWaves {
 		std::map <float, glm::vec3> mapPos;
 		float dis, sum = 0;
 		float mitjana = 0;
-		glm::vec3 Vsub;
+		float yEnfonsat;
 		glm::vec3 yNorm = glm::vec3(0, 1, 0);
+		float density = WATER;
 
 		for (int i = 0; i < LilSpheres::maxParticles; i++) {
 			dis = glm::distance(sphere->pos, glm::vec3(pC[i].pos.x, 0.f, pC[i].pos.z));
@@ -159,18 +158,29 @@ namespace GerstnerWaves {
 				mitjana = sum / 4;
 				break;
 			}
-			//to be continued
-			if(sphere->pos.y - mitjana > 0)
-				Vsub = glm::vec3(0, sphere->pos.y - mitjana, 0);
-			else Vsub = glm::vec3(0);
 		}
+		//checks if
+		if (sphere->pos.y - sphere->rad < mitjana) {
 
-		sphere->force = gravity*Vsub*yNorm;
-		sphere->force *= TEST;
+			yEnfonsat = glm::distance(sphere->pos.y - sphere->rad, mitjana);
+			float volumSub;
+			volumSub = yEnfonsat * pow(sphere->rad, 2);
+			sphere->force = density * glm::abs(gravity) * volumSub * yNorm;
 
+			//drag force section (nice joke xd!)
+				glm::vec3 dragForce;
+				//module of relative force
+				float moduloVel = glm::sqrt(glm::pow(sphere->vel.x, 2) + glm::pow(sphere->vel.y, 2) + glm::pow(sphere->vel.z, 2));
+				//applying the formula
+				dragForce = -0.5f * density * cD * glm::abs(volumSub) *  moduloVel * sphere->vel;
+				sphere->force += dragForce;
+			
+		}
+		//classic fm
+		else sphere->force = glm::vec3(0, gravity * sphere->mass, 0);
+
+		
 	}
-
-
 }
 
 
@@ -183,17 +193,29 @@ void PhysicsInit() {
 	sphere->rad = ((float)rand() / RAND_MAX) * 0.5f + 1;
 	
 
-	for (int i = 0; i < lengthWaves; i++) {
+	float randFreq = ((float)rand() / RAND_MAX);
+	//1a ona
 
-		float randFreq = ((float)rand() / RAND_MAX);
-		glm::vec3 randK = glm::vec3(((float)rand() / RAND_MAX), 0.f, ((float)rand() / RAND_MAX));
-		float randAmpl = ((float)rand() / RAND_MAX);
-			waves[i].amplitude = randAmpl + i;
-			waves[i].freq = randFreq + i;
-			waves[i].phi = ((float)rand() / RAND_MAX);
-			waves[i].k = randK;
+	waves[0].amplitude = 1.0;
+	waves[0].freq = randFreq * 2;
+	waves[0].phi = 0.f;
+	waves[0].k = glm::vec3(0.5f, 0, 0.2f);
 
-	}
+	//2a ona
+	waves[1].amplitude = 0.8;
+	waves[1].freq = randFreq * 1;
+	waves[1].phi = 2.f;
+	waves[1].k = glm::vec3(0.1f, 0, 0.6f);
+	//3a ona
+	waves[2].amplitude = 0.9;
+	waves[2].freq = randFreq * 3;
+	waves[2].phi = 0.f;
+	waves[2].k = glm::vec3(0.1f, 0, 0.6f);
+	//4a ona
+	waves[3].amplitude = 0.3;
+	waves[3].freq = randFreq * 5;
+	waves[3].phi = 3.f;
+	waves[3].k = glm::vec3(0.8f, 0, 0.3f);
 	//Grid
 	for (int i = 0; i < LilSpheres::maxParticles; ++i) {
 		
@@ -205,7 +227,7 @@ void PhysicsInit() {
 		pC[i].force = glm::vec3(0.f, 0.f, 0.f);
 		pC[i].posInitial = glm::vec3(pC[i].pos.x, pC[i].pos.y, pC[i].pos.z);
 	}
-	sphere->linealMomentum = glm::vec3(0, 0, 0);
+	sphere->linealMomentum = glm::vec3(0, -9.8f, 0);
 	ClothMesh::updateClothMesh(mesh);
 }
 
@@ -231,7 +253,7 @@ void PhysicsUpdate(float dt) {
 	}
 	GerstnerWaves::Buoyancy();
 
-	sphere->linealMomentum = sphere->linealMomentum + sphere->force;
+	sphere->linealMomentum = sphere->linealMomentum + dt * sphere->force;
 	sphere->vel = sphere->linealMomentum / sphere->mass;
 	sphere->pos = sphere->pos + dt * sphere->vel;
 
